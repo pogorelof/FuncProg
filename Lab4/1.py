5d29a23b39558b46fcc9be0ba802a71d008fb280
pogorelof <v.pogorelof@gmail.com>
2023-02-06 14:18:33 +0600

    lab3

---
+++ Lab4/1.py
@@ -0,0 +1,52 @@
+string = 'Hello'
+#1
+print('#1 count()')
+find = 'l'
+length = string.count(find)
+print(f'Количество букв {find}: {length} \n')
+
+#2
+print('#2 lower()')
+string = string.lower()
+print(string + '\n')
+
+#3
+print('#3 capitalize()')
+string = string.capitalize()
+print(string + '\n')
+
+#4
+print('#4 upper()')
+string = string.upper()
+print(string + '\n')
+
+#5
+print('#5 конкатенция')
+string_2 = ' World'
+string_3 = string + string_2
+print(string_3 + '\n')
+
+#6
+print('#6 replace()')
+string = string.replace('L', '24')
+print(string + '\n')
+
+#7
+print('#7 index()')
+index_of_E = string.index('E')
+print(str(index_of_E) + '\n')
+
+#8
+print('#8 split()')
+string = 'h_e_l_l_o'
+string_list = string.split('_')
+print(string_list)
+print()
+
+#9
+print('#9 title()')
+print(string.title() + '\n')
+
+#10
+print('#10 endswith()')
+print(string.endswith('o'))
---
+++ Lab4/2.py
@@ -0,0 +1,35 @@
+def add_student(name, num_class):
+    list_of_names.append(name)
+    list_of_class.append(num_class)
+
+def show_student(list_of_names, list_of_class):
+    for i in range(len(list_of_names)):
+        print(f'Ученик: {list_of_names[i]}')
+        print(f'Класс: {list_of_class[i]}')
+    
+
+def bubble_sort(nonsort_list, list_names):
+    size = len(nonsort_list)
+    for i in range(size - 1):
+        for j in range(size - i - 1):
+            if nonsort_list[j] > nonsort_list[j+1]:
+                nonsort_list[j], nonsort_list[j+1] = nonsort_list[j+1], nonsort_list[j]
+                list_names[j], list_names[j+1] = list_names[j+1], list_names[j]
+
+list_of_names = []
+list_of_class = []
+
+add_student('Vladimir', 11)
+add_student('Daniyar', 9)
+add_student('Danil', 10)
+add_student('Malika', 5)
+add_student('Adi', 11)
+add_student('Ernar', 6)
+add_student('Dulat', 1)
+
+print('До сортировки:')
+show_student(list_of_names, list_of_class)
+print()
+print('После сортировки:')
+bubble_sort(list_of_class, list_of_names)
+show_student(list_of_names, list_of_class)
