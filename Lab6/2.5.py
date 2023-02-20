bfee2c8f8a4418684a090dc5167fece046e919bb
pogorelof <v.pogorelof@gmail.com>
2023-02-20 18:23:24 +0600

    Lab6

--- Lab6/2.5.py
+++ Lab6/2.5.py
@@ -1,12 +1,12 @@
-list_of_kiril = ['А','Ә','Б','В','Г','Ғ','Д','Е','Ё','Ж','З','И','Й','К',
+list_of_kiril = ('А','Ә','Б','В','Г','Ғ','Д','Е','Ё','Ж','З','И','Й','К',
                 'Қ','Л','М','Н','Ң','О','Ө','П','Р','С','Т','У','Ұ','Ү','Ф','Х','Һ',
-                'Ц','Ш','Щ','Ы','І', 'Э', 'Я']
-list_of_kiril_lowercase = [letter.lower() for letter in list_of_kiril]
+                'Ц','Ш','Щ','Ы','І', 'Э', 'Я')
+list_of_kiril_lowercase = (letter.lower() for letter in list_of_kiril)
 
-list_of_latin = ['А','Ä','B','V','G','Ğ','D','E','E','J','Z','I','I','K',
+list_of_latin = ('А','Ä','B','V','G','Ğ','D','E','E','J','Z','I','I','K',
                 'Q','L','M','N','Ñ','O','Ö','P','R','S','T','U','Ū','Ü','F','H','H',
-                'C','Ş','Ş','Y','I', 'E', 'YA']
-list_of_latin_lowercase = [letter.lower() for letter in list_of_latin]
+                'C','Ş','Ş','Y','I', 'E', 'YA')
+list_of_latin_lowercase = (letter.lower() for letter in list_of_latin)
 
 kazakh_text = '''«Оғыз қаған» дастаны – түркітектес халықтардың ежелгі шежіресін генеологиялық 
 аңыздар негізінде көркем тілмен баяндайтын эпостық туынды. «Оғыз» сөзінің мағынасы уыз, уыздас, 
@@ -29,4 +29,3 @@ for i in range(len(kazakh_text)):
 
 print('\nТекст на латинице: ')
 print(kazakh_text_str)
-        
