95965f9869ce039a42834653d93a0831099fec8b
pogorelof <v.pogorelof@gmail.com>
2023-02-20 19:43:36 +0600

    Lab6

--- Lab6/2.4.py
+++ Lab6/2.4.py
@@ -7,9 +7,4 @@ for i in range(len(students)):
     if not students[i].find(find) == -1:
         students_with_find.append(students[i])
 
-str_of_students = ''
-for i in range(len(students_with_find)):
-    str_of_students += students_with_find[i]
-    str_of_students += ' '
-
-print(str_of_students)
+print(*students_with_find)
